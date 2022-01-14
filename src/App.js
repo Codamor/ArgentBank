1f85da54b06393ea19c9a12ef4406a3b51a7f9c1
Adelino ARAUJO <araujo_adelino@orange.fr>
2022-01-14 15:23:36 +0100

    add Routes to App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Navigation from "./Components/Navigation/Navigation";
-import Hero from "./Components/Hero/Hero";
-import Feature from "./Components/Features/Feature";
-import Item from "./Components/Item/Item";
-
-import chatIcon from "./assets/img/icon-chat.png";
-import moneyIcon from "./assets/img/icon-money.png" ;
-import securityIcon from "./assets/img/icon-security.png" ;
 import Footer from "./Components/Footer/Footer";
 import HomePage from "./Pages/HomePage";
+import SignInPage from "./Pages/SignInPage";
+import UserPage from "./Pages/UserPage/UserPage";
 
 
 
@@ -18,26 +13,11 @@ function App() {
       <BrowserRouter>
           <Navigation />
 
-          <HomePage>
-              <Hero />
-              <Feature>
-                  <Item
-                      icon={chatIcon}
-                      title={"You are our #1 priority"}
-                      text={"Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes. "}
-                  />
-                  <Item
-                      icon={moneyIcon}
-                      title={"More savings means higher rates"}
-                      text={" The more you save with us, the higher your interest rate will be! "}
-                  />
-                  <Item
-                      icon={securityIcon}
-                      title={"Security you can trust"}
-                      text={"We use top of the line encryption to make sure your data and money is always safe. "}
-                  />
-              </Feature>
-          </HomePage>
+          <Routes>
+              <Route exact path="/" element={<HomePage />} />
+              <Route path="/sign-in" element={<SignInPage />} />
+              <Route path="/user" element={<UserPage />} />
+          </Routes>
 
           <Footer text={"Copyright 2022 Argent Bank"} />
 
