53773d408a6a70c89076e08f19c72367d5eb2fcd
Adelino ARAUJO <araujo_adelino@orange.fr>
2022-01-18 15:10:26 +0100

    add auth types

---
+++ src/store/Actions/AuthActions.js
@@ -0,0 +1,54 @@
+import {
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT,
+
+} from "../Types/AuthTypes";
+
+import AuthService from '../services/auth.service';
+
+export const signupNewUser = (email, password, firstName, lastName) => (dispatch) => {
+    return AuthService.signup(email, password, firstName, lastName).then(
+        () => {
+            dispatch({
+                type: SIGNUP_SUCCESS,
+            });
+
+            return Promise.resolve();
+        },
+        () => {
+            dispatch({
+                type: SIGNUP_FAIL,
+            });
+
+            return Promise.reject();
+        },
+    );
+};
+
+export const signinUser = (email, password) => (dispatch) => {
+    return AuthService.login({ email: email, password: password }).then(
+        () => {
+            dispatch({
+                type: LOGIN_SUCCESS,
+            });
+
+            return Promise.resolve();
+        },
+        () => {
+            dispatch({
+                type: LOGIN_FAIL,
+            });
+
+            return Promise.reject();
+        },
+    );
+};
+
+export const signoutUser = () => (dispatch) => {
+    AuthService.logout();
+
+    dispatch({
+        type: LOGOUT,
+    });
+};
