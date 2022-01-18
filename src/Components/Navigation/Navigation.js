e57e68269631b70f3da5676900fcda17894d4167
Adelino ARAUJO <araujo_adelino@orange.fr>
2022-01-18 15:57:50 +0100

    fix navigation link path

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -23,7 +23,7 @@ class Navigation extends React.Component{
                     <h1 className="sr-only">Argent Bank</h1>
                 </Link>
                 <div>
-                    <Link to="sign-in" className="main-nav-item">
+                    <Link to="signin" className="main-nav-item">
                         <i className="fa fa-user-circle"></i>
                         Sign In
                     </Link>
