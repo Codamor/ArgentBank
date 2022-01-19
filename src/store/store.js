6d4cf023a4d8dd650b64c4748e1b5aef8407990b
Adelino ARAUJO <araujo_adelino@orange.fr>
2022-01-19 11:30:09 +0100

    add user reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -3,14 +3,18 @@ import thunk from 'redux-thunk';
 import {composeWithDevTools} from "@redux-devtools/extension";
 import {initialState} from "./initialState";
 import authReducer from "./Reducers/AuthReducer";
+import {userReducer} from "./Reducers/UserReducer";
 
 const middleware = [thunk] ;
 
 const rootReducer = combineReducers(
     {
-        auth: authReducer
+        auth: authReducer,
+        userProfile: userReducer
     }
 )
 
 
 export const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware))) ;
+
+store.subscribe(()=> {console.log(store.getState())})
