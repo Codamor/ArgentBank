fa99e45c5515a36b8f7c49cb9c43de81e9d44585
Adelino ARAUJO <araujo_adelino@orange.fr>
2022-01-19 14:48:19 +0100

    set header user name styles

--- src/Pages/UserPage/UserPage.js
+++ src/Pages/UserPage/UserPage.js
@@ -1,5 +1,6 @@
 import React from "react";
-import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import PropTypes, {func} from "prop-types";
 
 import "./UserPage.css" ;
 
@@ -10,11 +11,21 @@ import "./UserPage.css" ;
  * @component
  */
 class UserPage extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            profileEdition: false,
+            firstName: this.props.firstName,
+            lastName: this.props.lastName,
+        };
+    }
+
     render() {
         return (
             <main className="main bg-dark">
                 <div className="header">
-                    <h1>Welcome back<br/>{this.props.name}</h1>
+                    <h1>Welcome back{this.props.name}</h1>
+                    <h2>{this.props.firstName} {this.props.lastName} !</h2>
                     <button className="edit-button">Edit Name</button>
                 </div>
                 <h2 className="sr-only">Accounts</h2>
@@ -30,5 +41,15 @@ class UserPage extends React.Component{
     }
 }
 
+function mapStateToProps(state) {
+    const { isLoggedIn } = state.auth;
+    const { firstName, lastName } = state.userProfile;
+    return {
+        isLoggedIn,
+        firstName,
+        lastName,
+    };
+}
 
-export default UserPage ;
+
+export default connect(mapStateToProps)(UserPage) ;
